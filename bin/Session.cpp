851058d711686630b8447214c759a6eff80e0e15
sagidahan94 <sagidaha@post.bgu.ac.il>
2019-11-22 20:08:36 +0200

    new configuration json

--- bin/Session.cpp
+++ bin/Session.cpp
@@ -1,7 +1,16 @@
 #include "../include/Session.h"
+#include <../include/json.hpp>
+#include <fstream>
+#include <iostream>
 
 //Constructors
-    Session::Session(const std::string &configFilePath) {}
+    Session::Session(const std::string &configFilePath) {
+    using json= nlohamann::json;
+    std::ifstream i(configFilePath);
+    json j;
+    i >> j;
+    std:: cout << j << std::endl;
+}
     Session::Session(const Session &other) {}
 
 //Methods:
